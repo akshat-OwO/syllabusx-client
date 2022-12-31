88257e00bd2ed81e477c009154a0faa6cf58a384
Akshat Singh <akshatsinghdelhi@gmail.com>
2022-12-31 23:05:56 +0530

    siteInfo & tweaks

--- src/components/siteInfo/Footer.jsx
+++ src/components/siteInfo/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
     return (
         <div className="footer">
-            <p>Credits: <a href="#" target="_blank" rel="noopener noreferrer">@akshat</a> <a href="#" target="_blank" rel="noopener noreferrer">@shourya</a> <a href="#" target="_blank" rel="noopener noreferrer">@sparsh</a></p>
+            <p>Credits: <a href="https://linktr.ee/akshatOwO" target="_blank" rel="noopener noreferrer">@akshat</a> <a href="https://linktr.ee/shouryapal" target="_blank" rel="noopener noreferrer">@shourya</a> <a href="https://linktr.ee/yom4n" target="_blank" rel="noopener noreferrer">@sparsh</a></p>
         </div>
     );
 }
