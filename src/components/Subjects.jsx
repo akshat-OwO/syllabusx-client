2594c2cb5c745d38d49ab4d27b552ad0e90b6ed9
Akshat Singh <akshatsinghdelhi@gmail.com>
2023-01-01 14:38:55 +0530

    merge conflict

--- src/components/Subjects.jsx
+++ src/components/Subjects.jsx
@@ -16,10 +16,7 @@ const Subjects = (props) => {
       <div className="subjects hidden">
       <div className="sub-header">
         <h1>Subjects</h1>
-<<<<<<< HEAD
-=======
         <p>Better to complete it now than later!</p>
->>>>>>> main
       </div>
       {!props.nullData ? (<div className="sub-wrapper">
         {
