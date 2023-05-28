df6a8caefe91684cfbb6235d9003d9b267022389
Akshat Singh <akshatsinghdelhi@gmail.com>
2023-05-28 23:21:39 +0530

    fix: sem options

--- src/components/formFields/SemButton.jsx
+++ src/components/formFields/SemButton.jsx
@@ -17,14 +17,14 @@ const SemButton = ({ sem, semShow, setSem, setSemShow }) => {
         <div className="option-wrapper sem-btn" id="sem-btn">
             <button className='btn' tabIndex={1} onClick={toggleSem}>{semShow}</button>
             <div className="options semesters hide">
-              <p data-show="1st" data-value="firstsemesters" onClick={selectSem}>1st</p>
-              <p data-show="2nd" data-value="secondsemesters" onClick={selectSem}>2nd</p>
-              <p data-show="3rd" data-value="thirdsemesters" onClick={selectSem}>3rd</p>
-              <p data-show="4th" data-value="fourthsemesters" onClick={selectSem}>4th</p>
-//               <p data-show="5th" data-value="fifthsemesters" onClick={selectSem}>5th</p>
-//               <p data-show="6th" data-value="sixthsemesters" onClick={selectSem}>6th</p>
-//               <p data-show="7th" data-value="seventhsemesters" onClick={selectSem}>7th</p>
-//               <p data-show="8th" data-value="eighthsemesters" onClick={selectSem}>8th</p>
+                <p data-show="1st" data-value="firstsemesters" onClick={selectSem}>1st</p>
+                <p data-show="2nd" data-value="secondsemesters" onClick={selectSem}>2nd</p>
+                <p data-show="3rd" data-value="thirdsemesters" onClick={selectSem}>3rd</p>
+                <p data-show="4th" data-value="fourthsemesters" onClick={selectSem}>4th</p>
+                {/* <p data-show="5th" data-value="fifthsemesters" onClick={selectSem}>5th</p>
+                <p data-show="6th" data-value="sixthsemesters" onClick={selectSem}>6th</p>
+                <p data-show="7th" data-value="seventhsemesters" onClick={selectSem}>7th</p>
+                <p data-show="8th" data-value="eighthsemesters" onClick={selectSem}>8th</p> */}
             </div>
         </div>
     );
