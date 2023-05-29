fe929ac808eec68f80233bf5390e33471789fcbd
Akshat Singh <akshatsinghdelhi@gmail.com>
2023-05-29 22:00:15 +0530

    changes: new no data component with cat mascot

--- src/components/NoData.jsx
+++ src/components/NoData.jsx
@@ -3,6 +3,7 @@ const NoData = () => {
         <div className="null-data">
             <h1>404</h1>
             <p>No Data Found!</p>
+            <img src="/syllx-mascot.webp" alt="" />
         </div>
     );
 }
