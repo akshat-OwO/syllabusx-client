6378fddc792f50875004e43fc29873a767a36b74
Akshat Singh <akshatsinghdelhi@gmail.com>
2023-06-28 22:21:17 +0530

    new render server

--- src/hooks/useData.jsx
+++ src/hooks/useData.jsx
@@ -3,15 +3,9 @@ import { useEffect } from "react";
 const useData = (setData, searching, sem, branch) => {
     useEffect(() => {
         const getData = async () => {
-          try {
-            const response = await fetch(`http://107.21.128.65:3000/${sem}/${branch}`);
-            const json = await response.json();
-            setData(json);
-          } catch(error) {
             const response = await fetch(`https://syllabusx-web-server.onrender.com/${sem}/${branch}`);
             const json = await response.json();
             setData(json);
-          }
         }
         getData();
       }, [searching, sem, branch]);
