0fda8a9942bcbcae5b163223e8b58d954242d75c
Akshat Singh <akshatsinghdelhi@gmail.com>
2023-06-28 19:30:24 +0530

    AWS EC2 server migration

--- src/hooks/useData.jsx
+++ src/hooks/useData.jsx
@@ -4,7 +4,7 @@ const useData = (setData, searching, sem, branch) => {
     useEffect(() => {
         const getData = async () => {
           try {
-            const response = await fetch(`https://iboard-web-server.cyclic.app/${sem}/${branch}`);
+            const response = await fetch(`http://107.21.128.65:3000/${sem}/${branch}`);
             const json = await response.json();
             setData(json);
           } catch(error) {
