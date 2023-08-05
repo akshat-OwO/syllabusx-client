c7da9668119d348b9a34dc6fda0b90c9175b729a
Akshat Singh <akshatsinghdelhi@gmail.com>
2023-08-05 14:25:40 +0530

    typescript migration

--- src/components/siteInfo/PopHeader.jsx
+++
@@ -1,9 +0,0 @@
-const PopHeader = () => {
-    return (
-        <div className="">
-            {/* <p>Currently available for <span>B.Tech</span> 2021-25 and Onwards</p> */}
-        </div>
-    );
-}
- 
-export default PopHeader;
