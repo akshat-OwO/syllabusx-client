89a28380a28aad2c5b27bf07bce2602c16fd9472
akshat <akshatsinghdelhi@gmail.com>
2024-01-19 13:23:39 +0530

    build: commitlint and husky setup
    ensuring contriubers comply with the commit conventions used in this repo

---
+++ commitlint.config.js
@@ -0,0 +1,11 @@
+// build: development changes - related to the build system
+// ci: development changes - related to the continuous integration and deployment system
+// docs: documentation changes
+// feat: production changes - new feature
+// fix: production changes - bug fix
+// perf: production changes - improve performance
+// refactor: development changes - neither adds a feature nor fixes a bug
+// style: development changes - pure styling changes
+// test: development changes - affecting any tests
+
+module.exports = { extends: ["@commitlint/config-conventional"] };
