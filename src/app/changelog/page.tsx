97f5f26af64c796fd9b420edc85b0902d50a477e
Shourya Pal <palshourya897@gmail.com>
2024-02-11 13:21:23 +0530

    refactor: changleog limit with view more

--- src/app/changelog/page.tsx
+++ src/app/changelog/page.tsx
@@ -28,21 +28,23 @@ export const metadata: Metadata = {
     },
 };
 
-interface pageProps {}
+interface pageProps { }
 
-async function getReleases() {
+async function getReleases(pageSize = 8, page = 1) {
     const response = await octokit.request(
-        "GET /repos/{owner}/{repo}/releases",
-        {
-            owner: "akshat-OwO",
-            repo: "syllabusx-client",
-        }
+      "GET /repos/{owner}/{repo}/releases",
+      {
+        owner: "akshat-OwO",
+        repo: "syllabusx-client",
+        per_page: pageSize, 
+        page, 
+      }
     );
-
+  
     return response.data;
-}
+  }
 
-const page: FC<pageProps> = async ({}) => {
+const page: FC<pageProps> = async ({ }) => {
     const releases = await getReleases();
 
     return (
@@ -58,8 +60,7 @@ const page: FC<pageProps> = async ({}) => {
                 </div>
                 <div className="prose prose-sm prose-neutral dark:prose-invert">
                     <p className="text-center">
-                        Your backstage pass to witness the heartbeat of academic
-                        innovation.
+                        Your backstage pass to witness the heartbeat of academic innovation.
                     </p>
                 </div>
             </div>
@@ -71,10 +72,17 @@ const page: FC<pageProps> = async ({}) => {
                         title={release.name}
                         href={release.html_url}
                     >
-                        Release Date:{" "}
-                        {format(release.published_at!, "dd/MM/yyyy")}
+                        Release Date: {format(release.published_at!, "dd/MM/yyyy")}
                     </ChangeLogCard>
                 ))}
+
+
+                <a href="https://github.com/akshat-OwO/syllabusx-client/releases" className="block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground">
+                    <div className="text-sm font-medium leading-none">View More</div>
+                    <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">See all releases on GitHub</p>
+                </a>
+
+
             </div>
         </LayoutWrapper>
     );
