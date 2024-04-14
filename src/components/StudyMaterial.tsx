e990555a37f979f020bb7969f482f358a129c7ac
akshat <akshatsinghdelhi@gmail.com>
2024-04-14 20:06:11 +0530

    feat: new study material UI/UX

--- src/components/StudyMaterial.tsx
+++ src/components/StudyMaterial.tsx
@@ -12,11 +12,10 @@ import {
     RefetchOptions,
     useQuery,
 } from "@tanstack/react-query";
-import { AlertCircle, Check, Download, Heart, RotateCw } from "lucide-react";
-import React, { useState } from "react";
+import { AlertCircle, Download, Heart, RotateCw } from "lucide-react";
+import { Icons } from "./Icons";
 import { Alert, AlertDescription, AlertTitle } from "./ui/alert";
-import { Badge } from "./ui/badge";
-import { Button, buttonVariants } from "./ui/button";
+import { Button } from "./ui/button";
 import { Skeleton } from "./ui/skeleton";
 import { TabsContent } from "./ui/tabs";
 
@@ -43,8 +42,6 @@ const StudyMaterial = ({
     branch,
     subject,
 }: StudyMaterialProps) => {
-    const [createFav, setCreateFav] = useState<boolean>(false);
-    const [download, setDownload] = useState<boolean>(false);
     const [favorites, setFavorites] = useLocalStorage<string[]>({
         key: "favorites",
         defaultValue: [],
@@ -64,28 +61,18 @@ const StudyMaterial = ({
         });
     };
 
-    const DownloadFile = (fileId: string) => {
+    const downloadFile = (fileId: string) => {
         window.open(
             `https://drive.google.com/uc?export=download&id=${fileId}`,
             "_blank"
         );
     };
 
-    const onClick = (d: Drive) => {
-        if (createFav) {
-            if (favorites.includes(d.id)) {
-                return removeFavorite(d.id);
-            }
-            return addFavorite(d.id);
+    const toggleFavorite = (d: Drive) => {
+        if (favorites.includes(d.id)) {
+            return removeFavorite(d.id);
         }
-        if (download) {
-            return DownloadFile(d.id);
-        }
-        return embed.onOpen({
-            embedLink: d.webViewLink.slice(0, -17) + "preview",
-            name: d.name.slice(0, -4),
-            embedId: d.id,
-        });
+        return addFavorite(d.id);
     };
 
     const generateQueryKey = (): QueryKey => {
@@ -125,35 +112,19 @@ const StudyMaterial = ({
 
     return (
         <TabsContent value={tab}>
-            <StudyMaterial.Header
-                error={error}
-                isFetching={isFetching}
-                isLoading={isLoading}
-                refetch={refetch}
-                download={download}
-                setDownload={setDownload}
-                createFav={createFav}
-                setCreateFav={setCreateFav}
-            />
+            <StudyMaterial.Header isFetching={isFetching} refetch={refetch} />
             {error ? <StudyMaterial.Error /> : null}
 
             {isLoading ? <StudyMaterial.Skeleton /> : null}
 
             {data && !error && (
-                <div className="grid grid-cols-2 gap-4 rounded-md bg-accent p-2 md:grid-cols-3 xl:grid-cols-4">
+                <div className="grid grid-cols-2 gap-4 rounded-md bg-accent p-1.5 md:grid-cols-3 xl:grid-cols-4">
                     {data.map((d) => (
-                        <Button
+                        <div
                             key={d.id}
-                            variant={
-                                embed.embedLink ===
-                                d.webViewLink.slice(0, -17) + "preview"
-                                    ? "ghost"
-                                    : favorites.includes(d.id)
-                                      ? "default"
-                                      : "tertiary"
-                            }
-                            className="group relative h-full min-h-[3rem] cursor-pointer whitespace-normal text-center font-semibold shadow-sm"
-                            onClick={() => onClick(d)}
+                            className={cn(
+                                "relative flex flex-col justify-center divide-y divide-secondary rounded-md"
+                            )}
                         >
                             {!(
                                 new Date(Date.parse(d.createdTime)).getTime() <
@@ -161,53 +132,62 @@ const StudyMaterial = ({
                                     Date.now() - 2 * 24 * 60 * 60 * 1000
                                 ).getTime()
                             ) && (
-                                <Badge
-                                    variant={"secondary"}
-                                    className="absolute -left-2 -top-2 z-10 rounded-sm bg-teal-600 hover:bg-teal-600 group-hover:animate-pulse"
-                                >
-                                    New
-                                </Badge>
+                                <div className="absolute left-1 top-1 h-2 w-2 animate-pulse rounded-full bg-primary" />
                             )}
-                            <div
+                            <span
+                                role="button"
+                                title={`${d.name}`}
                                 className={cn(
-                                    "absolute hidden h-full w-full items-center justify-center rounded-md bg-background/90 hover:bg-secondary/80",
+                                    "h-10 truncate rounded-t-md bg-background px-4 py-2 text-center text-sm font-semibold text-foreground transition-colors hover:bg-background/90",
                                     {
-                                        flex: createFav || download,
+                                        "bg-primary text-primary-foreground hover:bg-primary/90":
+                                            favorites.includes(d.id),
                                     }
                                 )}
+                                onClick={() =>
+                                    embed.onOpen({
+                                        embedLink:
+                                            d.webViewLink.slice(0, -17) +
+                                            "preview",
+                                        name: d.name.slice(0, -4),
+                                        embedId: d.id,
+                                    })
+                                }
                             >
-                                {createFav && (
-                                    <div
-                                        className={cn(
-                                            buttonVariants({
-                                                size: "icon",
-                                                variant: "tertiary",
-                                            })
-                                        )}
-                                    >
-                                        <Heart
-                                            className={cn("h-4 w-4", {
-                                                "fill-red-500 stroke-red-500":
-                                                    favorites.includes(d.id),
-                                            })}
-                                        />
-                                    </div>
-                                )}
-                                {download && (
-                                    <div
-                                        className={cn(
-                                            buttonVariants({
-                                                size: "icon",
-                                                variant: "tertiary",
-                                            })
-                                        )}
-                                    >
-                                        <Download className="h-4 w-4" />
-                                    </div>
-                                )}
+                                {d.name.slice(0, -4)}
+                            </span>
+                            <div className="flex w-full items-center justify-start divide-x divide-secondary rounded-b-md">
+                                <div
+                                    role="button"
+                                    title={`Vote ${d.name}`}
+                                    className="flex flex-1 items-center gap-2 rounded-bl-md bg-background/75 px-2 py-1 font-semibold text-secondary-foreground hover:bg-background/60"
+                                >
+                                    <Icons.clap className="h-4 w-4 fill-primary" />{" "}
+                                    {0}
+                                </div>
+                                <div
+                                    role="button"
+                                    title={`Add ${d.name} to favorites`}
+                                    className="flex h-full items-center bg-background/75 px-3 py-1 font-semibold text-secondary-foreground hover:bg-background/60"
+                                    onClick={() => toggleFavorite(d)}
+                                >
+                                    <Heart
+                                        className={cn("h-4 w-4", {
+                                            "fill-red-500 stroke-red-500 transition-colors":
+                                                favorites.includes(d.id),
+                                        })}
+                                    />
+                                </div>
+                                <div
+                                    role="button"
+                                    title={`Download ${d.name}`}
+                                    className="flex h-full items-center rounded-br-md bg-background/75 px-3 py-1 font-semibold text-secondary-foreground hover:bg-background/60"
+                                    onClick={() => downloadFile(d.id)}
+                                >
+                                    <Download className="h-4 w-4" />
+                                </div>
                             </div>
-                            {d.name.slice(0, -4)}
-                        </Button>
+                        </div>
                     ))}
                 </div>
             )}
@@ -252,58 +232,20 @@ StudyMaterial.Error = function StudyMaterialError() {
 };
 
 StudyMaterial.Header = function StudyMaterialHeader({
-    error,
-    isLoading,
     isFetching,
     refetch,
-    download,
-    setDownload,
-    createFav,
-    setCreateFav,
 }: {
-    error: Error | null;
-    isLoading: boolean;
     isFetching: boolean;
     refetch: (
         options?: RefetchOptions | undefined
     ) => Promise<QueryObserverResult<Drive[] | null, Error>>;
-    download: boolean;
-    setDownload: (value: React.SetStateAction<boolean>) => void;
-    createFav: boolean;
-    setCreateFav: (value: React.SetStateAction<boolean>) => void;
 }) {
     return (
         <div className="mb-2 flex items-center justify-end gap-2">
             <Button
-                variant={!createFav ? "secondary" : "default"}
-                size={"icon"}
-                disabled={download || isLoading || !!error}
-                onClick={() => setCreateFav(!createFav)}
-            >
-                {createFav ? (
-                    <Check className="h-4 w-4" />
-                ) : (
-                    <Heart className="h-4 w-4" />
-                )}
-            </Button>
-
-            <Button
-                variant={!download ? "secondary" : "default"}
-                size={"icon"}
-                disabled={createFav || isLoading || !!error}
-                onClick={() => setDownload(!download)}
-            >
-                {download ? (
-                    <Check className="h-4 w-4" />
-                ) : (
-                    <Download className="h-4 w-4" />
-                )}
-            </Button>
-
-            <Button
                 variant={"secondary"}
                 size={"icon"}
-                disabled={isFetching || createFav || download}
+                disabled={isFetching}
                 onClick={() => refetch()}
             >
                 <RotateCw
